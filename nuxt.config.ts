bb1fc0c241addcead239a36c70f1fb19bf206c87
Ayodeji Alarape <ayodeji775@gmail.com>
2024-07-14 11:15:00 +0100

    chore : netlify oshi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,5 +81,17 @@ export default defineNuxtConfig({
     configPath: 'tailwind.config',
     viewer: false,
     quiet: true
+  },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      cssnano:
+        process.env.NODE_ENV === 'production'
+          ? { preset: ['default', { discardComments: { removeAll: true } }] }
+          : false, // disable cssnano when not in production
+    },
   }
+
 })
